ad2726ac163b4e981267f8fa16c88d2d1df59299
Maria Koval <marfantyk@gmail.com>
2023-04-10 00:47:33 +0300

    added animation and images changing

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let x = document.getElementById("x");
+var click = 0;
 x.onclick = function() {
-    x.parentElement.style.display="none";
+    let parent = x.parentElement;
+    if (parent.classList.contains("appearing")){
+        parent.classList.remove("appearing");
+        parent.classList.add("dissapearing");
+        setTimeout(()=> {
+            parent.style.display="none";
+            parent.classList.remove("dissapearing");
+        }, 300);
+
+    }
 }
 
 let clouds = document.getElementsByClassName("cloud-pop")
@@ -8,6 +18,16 @@ let clouds = document.getElementsByClassName("cloud-pop")
 for (let i = 0; i < clouds.length; i++){
     let cloud = clouds[i];
     cloud.onclick = function (){
-        document.getElementById("x").parentElement.style.display="block";
+        if (click < 5){
+            click= click + 1;
+        }
+        else{
+            click = 1;
+        }
+        let x = document.getElementById("x")
+        x.parentElement.style.display="block";
+        x.parentElement.classList.add("appearing");
+        console.log(click)
+        x.previousSibling.previousSibling.firstChild.nextSibling.src = "images/animal" + click + ".jpg"
     }
 }
